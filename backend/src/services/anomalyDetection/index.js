5418c3eb2cb98164ab8fdf6e739612735a257d72
doranali <doranali1234@gmail.com>
2026-06-03 14:54:11 +0300

    feat(anomaly): add orchestrator with cooldown and overrides

---
+++ backend/src/services/anomalyDetection/index.js
@@ -0,0 +1,49 @@
+const defaultThresholds = require('../../config/thresholds');
+const detectSuddenBrake = require('./suddenBrake');
+const detectSuddenAcceleration = require('./suddenAcceleration');
+const detectSharpTurn = require('./sharpTurn');
+const detectExcessiveJolt = require('./excessiveJolt');
+const { SlidingWindowStore } = require('./utils');
+
+const slidingWindowStore = new SlidingWindowStore();
+const cooldownMap = new Map();
+
+function keyFor(deviceId, type) {
+  return `${deviceId}:${type}`;
+}
+
+function inCooldown(deviceId, type, cooldownMs) {
+  const key = keyFor(deviceId, type);
+  const last = cooldownMap.get(key);
+  if (!last) return false;
+  return Date.now() - last < cooldownMs;
+}
+
+function setCooldown(deviceId, type) {
+  cooldownMap.set(keyFor(deviceId, type), Date.now());
+}
+
+function mergeThresholds(override = {}) {
+  return { ...defaultThresholds, ...override };
+}
+
+function analyzeBatch({ deviceId, readings, thresholdOverride }) {
+  const thresholds = mergeThresholds(thresholdOverride);
+  const windowMs = thresholds.slidingWindowSec * 1000;
+  const windowReadings = slidingWindowStore.push(deviceId.toString(), readings, windowMs);
+
+  const alarms = [
+    ...detectSuddenBrake(windowReadings, thresholds),
+    ...detectSuddenAcceleration(windowReadings, thresholds),
+    ...detectSharpTurn(windowReadings, thresholds),
+    ...detectExcessiveJolt(windowReadings, thresholds),
+  ].filter((alarm) => {
+    if (inCooldown(deviceId, alarm.type, thresholds.eventCooldownMs)) return false;
+    setCooldown(deviceId, alarm.type);
+    return true;
+  });
+
+  return alarms;
+}
+
+module.exports = { analyzeBatch };
